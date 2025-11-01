8d06969b757c3461c7db1b3e1dc2ee159e12e5f5
Jan Sramek <jan.sramek.ix@gmail.com>
2025-11-01 10:04:29 +0100

    better script

--- scripts/fmt-nightly.cmd
+++ scripts/fmt-nightly.cmd
@@ -1,4 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
 cd ..
+
+set RUSTFMT_FILE=rustfmt.toml
+set BACKUP_FILE=%RUSTFMT_FILE%.bak
+
+echo Backing up %RUSTFMT_FILE%...
+copy "%RUSTFMT_FILE%" "%BACKUP_FILE%" >nul
+
+echo Uncommenting settings...
+powershell -Command "(Get-Content '%RUSTFMT_FILE%') -replace '^(#\s*)(imports_granularity|group_imports)', '$2' | Set-Content '%RUSTFMT_FILE%'"
+
+echo Running cargo fmt...
 cargo +nightly fmt
 
+echo Restoring original %RUSTFMT_FILE%...
+move /Y "%BACKUP_FILE%" "%RUSTFMT_FILE%" >nul
+
+echo Done.
 pause
